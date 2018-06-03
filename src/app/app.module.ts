c654b0caa7b28637741a8fae828c9bc47ad5aaa0
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-06-03 09:10:47 +0100

    Loading the router library + forms library. Importing the components and declare them so they are available to use. Configuring the router module with RouterModule.forRoot. Handling invalid routes with a wildcard route. Navigating to the default route when the app launches with an empty path.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import {CrisisListComponent} from './crisis-list/crisis-list.component';
 import {HeroesListComponent} from './heroes-list/heroes-list.component';
-import {PageNotFoundComponent} from './not-found.component';
+import {PageNotFoundComponent} from './page-not-found.component';
 
 
 const appRoutes: Routes = [
