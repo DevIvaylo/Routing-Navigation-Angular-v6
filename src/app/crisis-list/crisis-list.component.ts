b22df9ac64e7f13c58f8b1d0e0af16f566058be4
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-06-03 09:13:52 +0100

    super basic inline template for demo purposes..

---
+++ src/app/crisis-list/crisis-list.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-crisis-list',
+  template: `
+    <p>Crisis Center Works</p>`,
+
+})
+export class CrisisListComponent {
+
+  constructor() {
+  }
+
+}
