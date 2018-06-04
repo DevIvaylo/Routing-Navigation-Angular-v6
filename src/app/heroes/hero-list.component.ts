d6a9b575d477d99937d646294cc1078e2656c7a9
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-06-04 08:57:21 +0100

    Importing router, activated route, paramMap, switchMap, hero service and hero interface, observables. Creating inline template. Injecting the services to constructor. Adding method goToHeroes().

---
+++ src/app/heroes/hero-list.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {switchMap} from 'rxjs/internal/operators';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+
+import {Hero, HeroService} from './hero.service';
+
+@Component({
+  selector: 'app-hero-list',
+  template: `
+    <h2>HEROES</h2>
+    <ul class="items">
+      <li *ngFor="let hero of heroes$ | async"
+          [class.selected]="hero.id === selectedId">
+        <a [routerLink]="['/hero', hero.id]">
+          <span class="badge">{{ hero.id }}</span>{{ hero.name }}
+        </a>
+      </li>
+    </ul>
+
+    <button routerLink="/sidekicks">Go to sidekicks</button>
+  `,
+})
+export class HeroListComponent implements OnInit {
+
+  heroes$: Observable<Hero[]>;
+  public selectedId: number;
+
+  constructor(private service: HeroService,
+              private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.heroes$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        // (+) before `params.get()` turns the string into a number
+        this.selectedId = +params.get('id');
+        return this.service.getHeroes();
+      })
+    );
+  }
+
+
+}
