67347b7f6208e3daa63309b2d215302117a82f7d
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-06-04 08:48:58 +0100

    Importing heroes module and browser animation module into the root module and moving heroes list component to heroes module..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,30 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HeroesModule} from './heroes/heroes.module';
 import {AppRoutingModule} from './app-routing.module';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 // components
 import {AppComponent} from './app.component';
 import {CrisisListComponent} from './crisis-list/crisis-list.component';
-import {HeroesListComponent} from './heroes-list/heroes-list.component';
 import {PageNotFoundComponent} from './page-not-found.component';
-
-
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent,
-    PageNotFoundComponent,
-
+    PageNotFoundComponent
   ],
   imports: [
+    // Notice that the AppRoutingModule is last. Most importantly, it comes after the HeroesModule.
+    // The order of route configuration matters. The router accepts the first route that matches a navigation request path.
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
+    HeroesModule,
+    BrowserAnimationsModule,
+    AppRoutingModule
 
   ],
   providers: [],
