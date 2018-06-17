26d44099ad3f49139d398e77613640ba4ca55110
Ivaylo Gospodinov <ivaylo.igconsultancyservices@gmail.com>
2018-06-17 10:25:41 +0100

    importing the loading bar service and injecting it into constructor. As well adding chnge detection strategy on push..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectionStrategy} from '@angular/core';
+import {LoadingBarService} from '@ngx-loading-bar/core';
+
 
 @Component({
   selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    :host {
+      padding: 64px 32px;
+      display: block;
+
+    }
+  `]
 })
 export class AppComponent {
+
+  constructor(public loader: LoadingBarService) {
+  }
+
 
 }
